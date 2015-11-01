13db300cde3da2db3a939140fcb2bd58da2bb0d3
julien.manganne <julien.manganne@extreme-sensio.com>
2015-11-01 14:47:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# YamlConfigServiceProvider
+
+Service provider for Silex ```v2.x``` using YAML configuration files.
+
+## Installation
+
+To use it add following line to your composer.json:
+
+    "require": {
+        ...
+        "juuuuuu/yaml-config-service-provider": "1.0.x-dev"
+        ...
+    }
+
+
+## Usage
+
+Include following line of code somewhere in your initial Silex file (index.php or whatever):
+
+    $app->register(new Juuuuuu\Silex\YamlConfigServiceProvider(__DIR__.'/../app/config/parameters.yml'));
+
+Now you have access to all of your configuration variables through `$app['parameters']`.
+
+
+## Example
+
+parameters.yml:
+
+```yml
+parameters:
+    webservice:
+        url: http://host.com
+        username: user
+        password: pass
+```
+
+index.php:
+
+```php
+    <?php
+        require_once __DIR__.'/../vendor/autoload.php';
+
+        $app = new Silex\Application();
+
+        // Considering the parameters.yml files is in app/config directory
+        $app->register(new Juuuuuu\Silex\YamlConfigServiceProvider(__DIR__.'/../app/config/parameters.yml'));
+
+        $wsUrl = $app['parameters']['webservice']['url'];
+```
---
+++ src/Juuuuuu/Silex/YamlConfigServiceProvider.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Service;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+use Symfony\Component\Yaml\Parser;
+
+class YamlConfigServiceProvider implements ServiceProviderInterface
+{
+    private $filename;
+    private $parser;
+
+    function __construct($filename) {
+        $this->filename = $filename;
+        $this->parser = new Parser();
+    }
+
+    public function register(Container $app)
+    {
+        $config = $this->getConfig();
+        $this->merge($app, $config);
+    }
+
+    private function getConfig()
+    {
+        if (!$this->filename) {
+            throw new \RuntimeException('A valid configuration file must be passed before reading the config.');
+        }
+
+        if (!file_exists($this->filename)) {
+            throw new \InvalidArgumentException(
+                sprintf("The config file '%s' does not exist.", $this->filename));
+        }
+
+        return $this->load($this->filename);
+
+        throw new \InvalidArgumentException(
+                sprintf("The config file '%s' appears to have an invalid format.", $this->filename));
+    }
+
+    private function load($filename)
+    {
+        if (!class_exists('Symfony\\Component\\Yaml\\Yaml')) {
+            throw new \RuntimeException('Unable to read yaml as the Symfony Yaml Component is not installed.');
+        }
+
+        $config = $this->parser->parse(file_get_contents($this->filename));
+
+        return $config ?: array();
+    }
+
+    private function merge(Container $app, array $config)
+    {
+        foreach ($config as $name => $value) {
+            $app[$name] = $value;
+        }
+    }
+}
